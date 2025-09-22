83f8ad051c02e395d380ac54d9841e903ddfe43e
MathHVReis <mathreis3110@gmail.com>
2025-09-22 10:32:50 -0300

    V3
    Destruindo colliders para mover a vaga - NAO ESTA MOVENDO A VAGA_DESTAQUE

--- Meu_Primeiro_Jogo/Assets/PROMETEO - Car Controller/Scripts/Contato_Vaga.cs
+++ Meu_Primeiro_Jogo/Assets/PROMETEO - Car Controller/Scripts/Contato_Vaga.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class Contato_Vaga : MonoBehaviour {
     public int score = 0;
+
+    // Referência para o script do GameManager
     public GameManager gameManager;
 
     private void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Vaga_Destaque")) {
+        // Verifica se o objeto que o carro encostou tem a tag "Collider_Destaque"
+        if (other.CompareTag("Collider_Destaque")) {
             score += 1;
             Debug.Log("Carro estacionou! Pontuação: " + score);
 
-            // Apenas avisa ao GameManager que uma vaga foi usada
-            gameManager.VagaEstacionada();
+            // Destrói o objeto do collider com o qual o carro colidiu
+            Destroy(other.gameObject);
+
+            // Avisa ao GameManager para escolher um novo alvo
+            gameManager.EscolherProximaVaga();
         }
     }
 }
--- Meu_Primeiro_Jogo/Assets/PROMETEO - Car Controller/Scripts/GameManager.cs
+++ Meu_Primeiro_Jogo/Assets/PROMETEO - Car Controller/Scripts/GameManager.cs
@@ -2,45 +2,25 @@ using UnityEngine;
 using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
-    // Lista de todas as posições possíveis para as vagas
-    public Transform[] posicoesPossiveis;
 
-    // Lista para guardar as posições disponíveis
-    private List<Transform> posicoesDisponiveis;
+    // Este método é chamado pelo carro para escolher a próxima vaga
+    public void EscolherProximaVaga() {
+        // Encontra todos os objetos com a tag "Collider_Vaga"
+        GameObject[] vagasDisponiveis = GameObject.FindGameObjectsWithTag("Collider_Vaga");
 
-    // Esta função é chamada pelo script do carro
-    public void VagaEstacionada() {
-        // Passo 1: Encontra todas as vagas com a tag "Vaga_Destaque"
-        GameObject[] vagasParaMover = GameObject.FindGameObjectsWithTag("Vaga_Destaque");
+        // Checa se há vagas disponíveis para escolher
+        if (vagasDisponiveis.Length > 0) {
+            // Escolhe uma vaga aleatória da lista
+            int indiceAleatorio = Random.Range(0, vagasDisponiveis.Length);
+            GameObject novaVagaAlvo = vagasDisponiveis[indiceAleatorio];
 
-        // Passo 2: Prepara e embaralha a lista de posições disponíveis
-        posicoesDisponiveis = new List<Transform>(posicoesPossiveis);
-        Shuffle(posicoesDisponiveis);
+            // Muda a tag da nova vaga para "Collider_Destaque"
+            novaVagaAlvo.tag = "Collider_Destaque";
 
-        // Passo 3: Move cada uma das vagas encontradas para uma nova posição
-        for (int i = 0; i < vagasParaMover.Length; i++) {
-            if (i < posicoesDisponiveis.Count) {
-                // Acha o objeto principal da vaga (pai, se o collider for um filho)
-                Transform vagaCompleta = vagasParaMover[i].transform.parent != null ? vagasParaMover[i].transform.parent : vagasParaMover[i].transform;
-
-                // Move a vaga para a nova posição
-                vagaCompleta.position = posicoesDisponiveis[i].position;
-                vagaCompleta.rotation = posicoesDisponiveis[i].rotation;
-            }
-            else {
-                Debug.LogWarning("Mais vagas do que posições disponíveis!");
-                break;
-            }
+            Debug.Log("Nova vaga de destaque: " + novaVagaAlvo.name);
         }
-    }
-
-    // Método para "embaralhar" a lista de posições
-    private void Shuffle(List<Transform> list) {
-        for (int i = 0; i < list.Count; i++) {
-            Transform temp = list[i];
-            int randomIndex = Random.Range(i, list.Count);
-            list[i] = list[randomIndex];
-            list[randomIndex] = temp;
+        else {
+            Debug.LogWarning("Não há mais vagas disponíveis para serem o alvo!");
         }
     }
 }
